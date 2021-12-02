a4611fa22e5159a7ccb595601114779b2d9b37be
favioloshow <faviocuyazevallos@gmail.com>
2021-12-02 01:08:11 -0500

    configuraciones 2

--- pregunta1/settings.py
+++ pregunta1/settings.py
@@ -125,8 +125,8 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'static')
-}
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
